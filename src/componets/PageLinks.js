a0c3583bda73b8132aecf01e4ff942afd8e8cf94
minhminh12315 <ddqminh.205@gmail.com>
2024-05-01 15:05:41 +0700

    fix PageLinks props

--- src/componets/PageLinks.js
+++ src/componets/PageLinks.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {pageLinks} from '../data'
 import PageLink from './PageLink'
-const PageLinks = (parentClass, itemClass) => {
+const PageLinks = ({parentClass, itemClass}) => {
   return (
     <ul className={parentClass}>
       {pageLinks.map((link) => {
