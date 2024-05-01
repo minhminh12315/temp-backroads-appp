24a9e52d7f4a10952fadfeeddb033c13c5213d9c
minhminh12315 <ddqminh.205@gmail.com>
2024-05-01 12:15:32 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './componets/Navbar'
+import Services from './componets/Services'
+import Tours from './componets/Tours'
+import Hero from './componets/Hero'
+import About from './componets/About'
+import Footer from './componets/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  )
+}
+
+export default App
